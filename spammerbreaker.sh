b771d26f10f2577b35c17e8c55de1c28d631c963
Tony Cardenas <antoniocal@gmail.com>
2019-02-15 23:02:26 -0600

    Added list function

--- spammerbreaker.sh
+++ spammerbreaker.sh
@@ -14,37 +14,45 @@ SL="$(sed -n 's/hostlist spammeripblocks = net-iplsearch;//p' $ECONF)"
 
 if [ -e "$SL" ] ; then 
 	
-	if [ $1 ] && [ $2 ]; then
-		if [[ $2 =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+	if [ $1 ] ; then
 			case $1 in
 				"rm" )
-					sed -i "/$2/d" $SL
-					if grep -Fxq "$2" $SL ; then
-						echo "IP could not be removed"
+					if [[ $2 =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+						sed -i "/$2/d" $SL
+						if grep -Fxq "$2" $SL ; then
+							echo "IP could not be removed"
+						else 
+							echo "IP successfully removed"
+						fi
 					else 
-						echo "IP successfully removed"
+						echo "Second argument is not an IP"
 					fi
 					;;
 			
 				"add" )
-					echo "$2" >> $SL
-					if grep -Fxq "$2" $SL ; then
-						echo "IP successfully added"
+					if [[ $2 =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+						echo "$2" >> $SL
+						if grep -Fxq "$2" $SL ; then
+							echo "IP successfully added"
+						else 
+							echo "IP could not be added"
+						fi
 					else 
-						echo "IP could not be added"
+						echo "Second argument is not an IP"
 					fi
+					;;
+				"list" )
+					cat $SL					
 					;;
 				
 				*)
 					echo "Coud not understand your arguments"
 				;;
 			esac
-		else 
-			echo "Second argument is not an IP"
-		fi
+	
 	else 
 	
-		if [ $1 == "restart" ]; then
+		if [[ $1 == "restart" ]] ; then
 			sh /scripts/restartsrv_exim
 		else 	
 			echo "No arguments passed, please pass [command] [IP]"
